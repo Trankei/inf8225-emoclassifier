c21140b4f828bf1019f3d6b825d8dd3dd1d10e59
Yousra Ben-Romdhane <benromdhane.yousra@gmail.com>
2019-04-21 01:28:47 -0400

    Updated early fusion not working yet
    Need to fix the criterion function to get the loss

--- code/early_fusion/earlyFusionModel.py
+++ code/early_fusion/earlyFusionModel.py
@@ -4,26 +4,26 @@ import torch.nn.functional as F
 from textToVector import TextToVector
 
 class EarlyFusionModel(nn.Module):
-    def __init__(self, image_to_vect_model, image_vect_dim, text_to_vect_model, text_vect_dim, num_classes):
+    def __init__(self, image_vect_dim, text_to_vect_model, text_vect_dim, num_classes):
         super(EarlyFusionModel, self).__init__()
-        self.image_to_vect = image_to_vect_model
         self.text_to_vect = text_to_vect_model
         self.linear_layer = nn.Linear(image_vect_dim + text_vect_dim, num_classes)
         self.softmax_layer = nn.Softmax()
         
-    def forward(self, image, text):
+    def forward(self, image_vect, text):
         # Convert image to vector
         # Modify code if not compatible with Keras image to vector model
-        image_vect = self.image_to_vect(image)
+        #image_vect = self.image_to_vect(image)
 
         # Convert text to vector
         # Note: input text should be a tensor of integers obtained from word dictionary lookup (see word_dictionary in earlyFusionTraining.py)
         text_vect = self.text_to_vect(text)
+        text_vect = text_vect.type(torch.LongTensor)
 
         # Concatenation of image and text vector (Fusion layer)
         concat_vect = torch.cat((image_vect, text_vect), dim=0)
         # Pass through linear layer
-        output = self.linear_layer(concat_vect)
+        output = self.linear_layer(concat_vect.type(torch.FloatTensor))
         # Softmax
         output = self.softmax_layer(output)
         return output
--- code/early_fusion/earlyFusionTraining.py
+++ code/early_fusion/earlyFusionTraining.py
@@ -1,7 +1,12 @@
 from earlyFusionModel import EarlyFusionModel
 from textToVector import TextToVector
+from textPreprocessing import preprocessText
+import torch.nn as nn
+import torch.optim as optim
 import gloveLoad
 import pickle
+import torch
+import numpy as np
 
 # Image dataset
 image_training_features = pickle.load(open('../../processed_data/image_training_features.pkl', 'rb'))
@@ -18,12 +23,16 @@ training_labels = pickle.load(open('../../processed_data/training_labels.pkl', '
 validation_labels = pickle.load(open('../../processed_data/validation_labels.pkl', 'rb'))
 testing_labels = pickle.load(open('../../processed_data/testing_labels.pkl', 'rb'))
 
+from sklearn.preprocessing import OneHotEncoder
+enc = OneHotEncoder()
+one_hot_training_labels = enc.fit_transform(training_labels)
+
 # Number of emotion classes
 num_classes = 4
 
-# TODO: Plug image to vector model
-image_to_vect_model = None
-# TODO: Plug image vector dim
+# Image to vector model
+# image_to_vect_model = pickle.load(open('../../models/image_classifier_71precision.pkl', 'rb')) #dont need it image already a feature
+# Image vector dim
 image_vector_dim = 2048
 
 # word_dictionary: Mapping words to index in vocabulary (to convert words to indexes before passing to model)
@@ -31,11 +40,39 @@ word_dictionary, pretrained_word_vectors = gloveLoad.create_dictionary(text_trai
 text_to_vect_model = TextToVector(pretrained_word_vectors)
 text_vector_dim = 200
 
-early_fusion_model = EarlyFusionModel(image_to_vect_model, image_vector_dim, text_to_vect_model, text_vector_dim, num_classes)
+early_fusion_model = EarlyFusionModel(image_vector_dim, text_to_vect_model, text_vector_dim, num_classes)
 
-# def train(model):
-#     # TODO: Implement training
-#     return model
+def train(model):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    for epoch in range(2):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, label in enumerate(one_hot_training_labels):
+            # get the inputs
+            image_feature = image_training_features[i]
+            tensor_image = torch.LongTensor(image_feature)
+            text = text_training[i][0]
+            text = preprocessText(text)
+            array_text = []
+            for word in text.split(): 
+                idx = word_dictionary[word]
+                array_text.append(idx)
+            tensor_text = torch.LongTensor(array_text)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # forward + backward + optimize
+            output = model(tensor_image, tensor_text)
+            idx = torch.tensor(np.argwhere(label[0] == 1)[0,1])
+            loss = criterion(output, idx)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 200 == 199:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %(epoch + 1, i + 1, running_loss / 200))
+                running_loss = 0.0
+    return model
 
 # def valid(model):
 #     # TODO: Implement validation
@@ -45,7 +82,7 @@ early_fusion_model = EarlyFusionModel(image_to_vect_model, image_vector_dim, tex
 #     # TODO: Implement testing
 #     return loss, precision
 
-
+early_fusion_model = train(early_fusion_model)
 
 
 
